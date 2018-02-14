631f7bd316d44d25d75d2efe6292b26a58921d03
Tabish Raza <tabishraza@gmail.com>
2018-02-14 16:16:29 +0500

    dynamic page using ejs templating engine

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express  = require('express');
-var app = express();
+var app  = require('express')();
+
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -7,6 +8,11 @@ app.get('/', function(req, res){
 
 app.get('/contact', function(req, res){
   res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/profile/:name', function(req, res){
+  var data = {age: 29, job: 'ninja'};
+  res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
