2f9a53f50cd7ecff92f6efa5d23a8a782af819bf
Tabish Raza <tabishraza@gmail.com>
2018-02-15 16:42:47 +0500

    using middleware for static css render

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var app  = require('express')();
+var express = require('express');
+var app  = express();
 
 app.set('view engine', 'ejs')
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
   res.render('index');
